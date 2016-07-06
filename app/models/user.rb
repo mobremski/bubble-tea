80abd652941677a7573d2855b56d15f1384aa62e
Shelly Stuart <shellzy@gmail.com>
2016-07-06 11:13:25 -0400

    fix shoulda matchers and add model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, uniqueness: true, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :encrypted_password, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,30 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "should validate the presence of First Name" do
-    should validate_presence_of(:first_name)
+    is_expected.to validate_presence_of(:first_name)
   end
 
   it "should validate the presence of Last Name" do
-    should validate_presence_of(:last_name)
+    is_expected.to validate_presence_of(:last_name)
   end
 
   it "should validate the presence of Email" do
-    should validate_presence_of(:email)
+    is_expected.to validate_presence_of(:email)
   end
 
   it "should validate the presence of Password" do
-    should validate_presence_of(:encrypted_password)
+    is_expected.to validate_presence_of(:encrypted_password)
   end
+
+  it { is_expected.to have_valid(:first_name).when("George") }
+  it { is_expected.to_not have_valid(:first_name).when(nil, "") }
+
+  it { is_expected.to have_valid(:last_name).when("Smith") }
+  it { is_expected.to_not have_valid(:last_name).when(nil, "") }
+
+  it { is_expected.to have_valid(:email).when("George@gmail.com") }
+  it { is_expected.to_not have_valid(:email).when(nil, "") }
+
+  it { is_expected.to_not have_valid(:encrypted_password).when(nil, "") }
+
 end
