148eddbc6fed4d353b249b55bfc332cdfdb6fcc4
Shelly Stuart <shellzy@gmail.com>
2016-07-06 16:07:13 -0400

    add shop unit tests

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,35 +1,11 @@
 require 'rails_helper'
-​
-describe Shop do
-  it "should validate presence of name" do
-    is_expected.to validate_presence_of(:name)
-  end
-​
-  it "should validate presence of address" do
-    is_expected.to validate_presence_of(:address)
-  end
-​
-  it "should validate presence of city" do
-    is_expected.to validate_presence_of(:city)
-  end
-​
-  it "should validate presence of state" do
-    is_expected.to validate_presence_of(:state)
-  end
-​
-  it "should validate presence of zip" do
-    is_expected.to validate_presence_of(:zip)
-  end
-​
-  it "should validate numericality of zip" do
-    is_expected.to validate_numericality_of(:zip)
-  end
-​
-  it "should validate length of zip" do
-    is_expected.to validate_length_of(:zip).is_equal_to(5)
-  end
-​
-  it "should have many reviews" do
-    should have_many(:reviews)
-  end
+
+RSpec.describe Shop, type: :model do
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:address) }
+  it { is_expected.to validate_presence_of(:city) }
+  it { is_expected.to validate_presence_of(:state) }
+  it { is_expected.to validate_presence_of(:zip) }
+  it { is_expected.to validate_numericality_of(:zip) }
+  it { is_expected.to validate_length_of(:zip).is_equal_to(5) }
 end
