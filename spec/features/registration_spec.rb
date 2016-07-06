f02b6aaab8c4f0fab3ea7671d0723125fa40ea0b
Shelly Stuart <shellzy@gmail.com>
2016-07-06 14:02:43 -0400

    get registration acceptance tests to pass

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -14,17 +14,17 @@ feature 'registration' do
     fill_in 'Last name', with: 'Smith'
     fill_in 'Email', with: 'user@example.com'
     fill_in 'Password', with: 'password'
-    fill_in 'Password Confirmation', with: 'password'
-    click_button 'Sign Up'
+    fill_in 'Password confirmation', with: 'password'
+    click_button 'Sign up'
 
-    expect(page).to have_content("You're in!")
-    expect(page).to have_content("Sign out")
+    expect(page).to have_content("You have signed up successfully")
+    expect(page).to have_content("Sign Out")
   end
 
   scenario 'required information not supplied' do
     visit root_path
     click_link 'Sign Up'
-    click_button 'Sign Up'
+    click_button 'Sign up'
 
     expect(page).to have_content("Email can't be blank")
     expect(page).to have_content("Password can't be blank")
@@ -35,17 +35,26 @@ feature 'registration' do
   scenario 'password confirmation does not match password' do
     visit root_path
     click_link 'Sign Up'
-    fill_in 'First Name', with: 'Jon'
-    fill_in 'Last Name', with: 'Smith'
+    fill_in 'First name', with: 'Jon'
+    fill_in 'Last name', with: 'Smith'
     fill_in 'Email', with: 'user@example.com'
     fill_in 'Password', with: 'password'
-    fill_in 'Password Confirmation', with: 'password1'
-    click_button 'Sign Up'
+    fill_in 'Password confirmation', with: 'password1'
+    click_button 'Sign up'
 
     expect(page).to have_content("Password confirmation doesn't match Password")
   end
 
   scenario 'password is too short' do
-    
+    visit root_path
+    click_link 'Sign Up'
+    fill_in 'First name', with: 'Jon'
+    fill_in 'Last name', with: 'Smith'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'pass'
+    fill_in 'Password confirmation', with: 'pass'
+    click_button 'Sign up'
+
+    expect(page).to have_content("Password is too short")
   end
 end
