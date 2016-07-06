eb5cfc1b00cb9a66766242b49e5bf3d8bc22e7b0
Michael Obremski <mobremski@gmail.com>
2016-07-06 10:51:38 -0400

    read functionality for shops

---
+++ spec/features/user_views_list_of_shops_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature "visitor sees a list of shops" do
+  scenario "sees a list of shops and link for new shop" do
+    bubblicious = Shop.create(name: 'Bubblicious', address: '33 Harrison Ave', city: 'Boston', state: 'MA', zip: '02111')
+    tapioca_town = Shop.create(name: "Tapioca Town", address: "Beach streer", city: "Boston", state: "MA", zip: "02112")
+
+    visit shops_path
+
+    expect(page).to have_content "Bubble Tea Shops"
+    expect(page).to have_content "Bubblicious"
+    expect(page).to have_link "Tapioca Town"
+
+    click_link "Add New Shop"
+
+    expect(page).to have_content "Add a Shop Location"
+  end
+
+  scenario "clicks link and is taken to show page for given shop" do
+    bubblicious = Shop.create(name: 'Bubblicious', address: '33 Harrison Ave', city: 'Boston', state: 'MA', zip: '02111', description: "Straightforward dumplings for a great price.")
+
+    visit root_path
+
+    click_link "Bubblicious"
+
+    expect(page).to have_content bubblicious.name
+    expect(page).to have_content bubblicious.address
+    expect(page).to have_content bubblicious.city
+    expect(page).to have_content bubblicious.state
+    expect(page).to have_content bubblicious.zip
+    expect(page).to have_content bubblicious.description
+  end
+end
