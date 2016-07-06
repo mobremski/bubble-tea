ab2b1d9e1fdd60b2494a6d86a90a208131462611
Michael Obremski <mobremski@gmail.com>
2016-07-06 11:23:02 -0400

    Fix errors from PR4

--- spec/features/user_views_list_of_shops_spec.rb
+++ spec/features/user_views_list_of_shops_spec.rb
@@ -2,13 +2,27 @@ require 'rails_helper'
 
 feature "visitor sees a list of shops" do
   scenario "sees a list of shops and link for new shop" do
-    bubblicious = Shop.create(name: 'Bubblicious', address: '33 Harrison Ave', city: 'Boston', state: 'MA', zip: '02111')
-    tapioca_town = Shop.create(name: "Tapioca Town", address: "Beach streer", city: "Boston", state: "MA", zip: "02112")
+    bubblicious = Shop.create(
+      name: "Bubblicious",
+      address: "33 Harrison Ave",
+      city: "Boston",
+      state: "MA",
+      zip: "02111",
+      description: "Straightforward Bubble Tea for a great price."
+      )
+
+    tapioca_town = Shop.create(
+      name: "Tapioca Town",
+      address: "Beach streer",
+      city: "Boston",
+      state: "MA",
+      zip: "02112"
+    )
 
     visit shops_path
 
     expect(page).to have_content "Bubble Tea Shops"
-    expect(page).to have_content "Bubblicious"
+    expect(page).to have_link "Bubblicious"
     expect(page).to have_link "Tapioca Town"
 
     click_link "Add New Shop"
@@ -17,7 +31,14 @@ feature "visitor sees a list of shops" do
   end
 
   scenario "clicks link and is taken to show page for given shop" do
-    bubblicious = Shop.create(name: 'Bubblicious', address: '33 Harrison Ave', city: 'Boston', state: 'MA', zip: '02111', description: "Straightforward dumplings for a great price.")
+    bubblicious = Shop.create(
+      name: "Bubblicious",
+      address: "33 Harrison Ave",
+      city: "Boston",
+      state: "MA",
+      zip: "02111",
+      description: "Straightforward Bubble Tea for a great price."
+      )
 
     visit root_path
 
