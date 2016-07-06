e74302df88be9d4a64a9123f1c04ec07f3078285
Michael Obremski <mobremski@gmail.com>
2016-07-06 10:20:39 -0400

    style-fixes in PR3

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,4 @@
 class ShopsController < ApplicationController
-
-
   def new
     @shop = Shop.new
   end
@@ -11,7 +9,7 @@ class ShopsController < ApplicationController
       flash[:notice] = "Shop added successfully"
       redirect_to shop_path(@shop)
     else
-      flash[:error] = @shop.errors.full_messages.join (', ')
+      flash[:error] = @shop.errors.full_messages.join(', ')
       render :new
     end
   end
@@ -25,5 +23,4 @@ class ShopsController < ApplicationController
   def shop_params
     params.require(:shop).permit(:name, :address, :city, :state, :zip, :description, :pricing, :hours)
   end
-
 end
