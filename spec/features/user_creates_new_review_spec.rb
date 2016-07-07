522e6bccce396a57417434d6c61af95894ce641c
Padrig Tuck <Padrig@gmail.com>
2016-07-07 15:45:09 -0400

    updates to crud review

--- spec/features/user_creates_new_review_spec.rb
+++ spec/features/user_creates_new_review_spec.rb
@@ -1,6 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 feature "user creates a new review" do
-  let!(:shop) {
+  let!(:shop) do
     Shop.create(
       name: "Boba",
       address: "33 harrison Avenue",
@@ -11,29 +11,28 @@ feature "user creates a new review" do
       pricing: "3-5 dollars",
       hours: "M-F all day"
     )
-  }
+  end
 
-
-  scenario "user visits new_shop_review path and successfully creates a new review" do
+  scenario "user successfully submits a new review" do
 
     visit root_path
-    click_link 'Sign Up'
-    fill_in 'First name', with: 'Jon'
-    fill_in 'Last name', with: 'Smith'
-    fill_in 'Email', with: 'user@example.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
-    click_button 'Sign up'
+    click_link "Sign Up"
+    fill_in "First name", with: "Jon"
+    fill_in "Last name", with: "Smith"
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
 
     expect(page).to have_content("You have signed up successfully")
     expect(page).to have_content("Sign Out")
 
-    click_link 'Boba'
-    click_link 'Add a Review'
+    click_link "Boba"
+    click_link "Add a Review"
 
     expect(page).to have_content("Review Form")
-    fill_in 'Rating', with: 5
-    fill_in 'Review', with: 'A middling BT spot'
-    click_button 'Add Review'
+    fill_in "Rating", with: 5
+    fill_in "Review", with: "A middling BT spot"
+    click_button "Add Review"
   end
 end
