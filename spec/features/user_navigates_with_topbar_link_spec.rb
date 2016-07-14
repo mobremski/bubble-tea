0fab17d5a65b5436946d08300bec59c94695d483
Shelly Stuart <shellzy@gmail.com>
2016-07-14 09:15:27 -0400

    fix failing test

--- spec/features/user_navigates_with_topbar_link_spec.rb
+++ spec/features/user_navigates_with_topbar_link_spec.rb
@@ -5,7 +5,7 @@ feature "User navigates to index page with top-bar link" do
   let!(:shop) { FactoryGirl.create(:shop, user_id: user.id) }
   scenario "User looks at shop page and clicks top link to go back to index" do
     visit shop_path(shop)
-    click_on "Bubble T-Stops"
+    find("#logo").click
     expect(page).to have_current_path(shops_path)
   end
 end
