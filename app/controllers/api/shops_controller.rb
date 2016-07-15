d51649ff9270d352f5706445105de789d457c07a
Michael Obremski <mobremski@gmail.com>
2016-07-15 12:01:39 -0400

    fixed search bar to work with react, added tests

--- app/controllers/api/shops_controller.rb
+++ app/controllers/api/shops_controller.rb
@@ -1,6 +1,7 @@
 class Api::ShopsController < ApiController
   def index
-    shops = Shop.all
+    shops = Shop.search(params[:query])
+    @shop = Shop.new
     render json: { shops: shops }, status: :ok
   end
 end
